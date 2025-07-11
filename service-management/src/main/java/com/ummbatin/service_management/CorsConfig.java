9fc45c3ed45c56ac76fa222b81432c8730d6ee4f
weaam-12 <abukafweaam2001@gmail.com>
2025-07-11 14:22:11 +0300

    cros allow for

--- service-management/src/main/java/com/ummbatin/service_management/CorsConfig.java
+++ service-management/src/main/java/com/ummbatin/service_management/CorsConfig.java
@@ -10,8 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:5173", "https://ummbatin-website.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
